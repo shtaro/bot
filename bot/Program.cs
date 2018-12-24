2906804bed4500624fa73a5f6a315889173aeb03
or shtar <orshtar30@gmail.com>
2018-12-24 13:35:51 +0200

    Add project files.

---
+++ bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
